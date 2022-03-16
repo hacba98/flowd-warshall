f4bd5ae373b1f8904d3d0842efe221fb5fd96769
Minh Pham <hacba98@gmail.com>
2022-03-16 10:38:55 +0700

    use port from env

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/', function (req, res) {
 
 
 // start web server
-app.listen(3000, () => {
-  console.log('Server is listening port 3000 ...');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening port ${port} ...`);
 });
